07c057c03b6b7f3d4298b4eaf180e81f562687b0
Teagan Glenn <that@teagantotally.rocks>
2019-05-27 13:31:10 -0600

    Custom components

---
+++ custom_components/my_sleepiq/binary_sensor.py
@@ -0,0 +1,56 @@
+"""
+Support for SleepIQ sensors.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sleepiq/
+"""
+from custom_components import my_sleepiq
+import logging
+
+DEPENDENCIES = ['my_sleepiq']
+ICON = 'mdi:hotel'
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the SleepIQ sensors."""
+    if discovery_info is None:
+        return
+
+    data = my_sleepiq.DATA
+    data.update()
+
+    dev = list()
+    for bed_id, bed in data.beds.items():
+        for side in my_sleepiq.SIDES:
+            dev.append(SleepNumberSensor(data, bed_id, side))
+    add_devices(dev)
+
+
+class SleepNumberSensor(my_sleepiq.SleepIQSensor):
+    """Implementation of a SleepIQ sensor."""
+
+    def __init__(self, sleepiq_data, bed_id, side):
+        """Initialize the sensor."""
+        my_sleepiq.SleepIQSensor.__init__(self, sleepiq_data, bed_id, side)
+
+        self._state = None
+        self.type = my_sleepiq.SLEEP_NUMBER
+        self._name = my_sleepiq.SENSOR_TYPES[self.type]
+
+        self.update()
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return ICON
+
+    def update(self):
+        """Get the latest data from SleepIQ and updates the states."""
+        my_sleepiq.SleepIQSensor.update(self)
+        self._state = self.side.sleep_number
